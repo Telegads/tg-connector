cea04c516066a98d70c155afe933c77a6871ce61
Nikitin Petr <nikitin.p.94@gmail.com>
2022-12-24 17:33:06 +0400

    add date to annotation

--- utils.py
+++ utils.py
@@ -5,16 +5,18 @@ import shutil
 def get_sessions():
     """ Возвращает пути ко всем файлам из папки sessions """
     sessions_paths = []
+   
     for session in os.listdir('sessions'):
         path = os.path.join('sessions', session)
         sessions_paths.append(path)
+    print(sessions_paths)
     return sessions_paths
 
 
 def mark_bad_session(session_file):
     """ Помещаем файл сессии в папку bad sessions """
     print(os.path.join(os.getcwd(), session_file))
-    
+
     shutil.move(os.path.join(os.getcwd(), session_file),
                os.path.join(os.getcwd(), session_file.replace('sessions', 'bad_sessions')))
 
